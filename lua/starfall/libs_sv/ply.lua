2bedf084fdb8fe646424ea6f6e2f1803deef9f8c
= <ProfessorBoxtrot@gmail.com>
2020-09-23 15:33:12 -0700

    Fix style errors

--- lua/starfall/libs_sv/ply.lua
+++ lua/starfall/libs_sv/ply.lua
@@ -21,7 +21,7 @@ local function getPly( this )
 end
 
 
-local function CanUsePlyCore( play )
+local function canUsePlyCore( play )
     return play:IsAdmin() or play:IsSuperAdmin()
 end
 
@@ -29,133 +29,133 @@ end
 --- Sets the health of a player.
 -- @param number Health
 function player_methods:setHealth( health )
-    if not CanUsePlyCore(instance.player) then SF.Throw("You must be an admin to use this function!") end
+    if not canUsePlyCore( instance.player ) then SF.Throw( "You must be an admin to use this function!" ) end
     
     checktype( self, ply_meta )
-    local ent = getPly( self )
-    ent:SetHealth( health )
+    local ply = getPly( self )
+    ply:SetHealth( health )
 end
 
 
 --- Sets the maximum health of a player. (i.e. How much health a Medkit can heal you to)
 -- @param number MaxHealth
 function player_methods:setMaxHealth( maxHealth )
-    if not CanUsePlyCore(instance.player) then SF.Throw("You must be an admin to use this function!") end
+    if not canUsePlyCore( instance.player ) then SF.Throw( "You must be an admin to use this function!" ) end
     
     checktype( self, ply_meta )
-    local ent = getPly( self )
-    ent:SetMaxHealth( maxHealth )
+    local ply = getPly( self )
+    ply:SetMaxHealth( maxHealth )
 end
 
 
 --- Sets the suit armor of a player.
 -- @param number SuitArmor
 function player_methods:setArmor( armor )
-    if not CanUsePlyCore(instance.player) then SF.Throw("You must be an admin to use this function!") end
+    if not canUsePlyCore( instance.player ) then SF.Throw( "You must be an admin to use this function!" ) end
     
     checktype( self, ply_meta )
-    local ent = getPly( self )
-    ent:SetArmor( armor )
+    local ply = getPly( self )
+    ply:SetArmor( armor )
 end
 
 
 --- Sets the jump power of a player. Default value is 200.
 -- @param number JumpPower
 function player_methods:setJumpPower( jumpPower )
-    if not CanUsePlyCore(instance.player) then SF.Throw("You must be an admin to use this function!") end
+    if not canUsePlyCore( instance.player ) then SF.Throw( "You must be an admin to use this function!" ) end
     
     checktype( self, ply_meta )
-    local ent = getPly( self )
-    ent:SetJumpPower( jumpPower )
+    local ply = getPly( self )
+    ply:SetJumpPower( jumpPower )
 end
 
 
 --- Sets the slow walking speed of a player. This is when you're using +WALK. Default value is 100.
 -- @param number WalkSpeed
 function player_methods:setSlowWalkSpeed( slowWalkSpeed )
-    if not CanUsePlyCore(instance.player) then SF.Throw("You must be an admin to use this function!") end
+    if not canUsePlyCore( instance.player ) then SF.Throw( "You must be an admin to use this function!" ) end
     
     checktype( self, ply_meta )
-    local ent = getPly( self )
-    ent:SetSlowWalkSpeed( slowWalkSpeed )
+    local ply = getPly( self )
+    ply:SetSlowWalkSpeed( slowWalkSpeed )
 end
 
 
 --- Sets the regular walking speed of a player. This is when you're just holding W/A/S/D and not using +WALK. Default value is 200.
 -- @param number WalkSpeed
 function player_methods:setWalkSpeed( walkSpeed )
-    if not CanUsePlyCore(instance.player) then SF.Throw("You must be an admin to use this function!") end
+    if not canUsePlyCore( instance.player ) then SF.Throw( "You must be an admin to use this function!" ) end
     
     checktype( self, ply_meta )
-    local ent = getPly( self )
-    ent:SetWalkSpeed( walkSpeed )
+    local ply = getPly( self )
+    ply:SetWalkSpeed( walkSpeed )
 end
 
 
 --- Sets the crouch speed multiplier of a player. This is a number from 0-1. Default is 0.3.
 -- @param number Multiplier
 function player_methods:setCrouchSpeedMultiplier( walkSpeed )
-    if not CanUsePlyCore(instance.player) then SF.Throw("You must be an admin to use this function!") end
+    if not canUsePlyCore( instance.player ) then SF.Throw( "You must be an admin to use this function!" ) end
     
     checktype( self, ply_meta )
-    local ent = getPly( self )
-    ent:SetCrouchedWalkSpeed( walkSpeed )
+    local ply = getPly( self )
+    ply:SetCrouchedWalkSpeed( walkSpeed )
 end
 
 
 --- Sets the running speed of a player. This is when you're holding SHIFT. Default value is 600.
 -- @param number RunSpeed
 function player_methods:setRunSpeed( runSpeed )
-    if not CanUsePlyCore(instance.player) then SF.Throw("You must be an admin to use this function!") end
+    if not canUsePlyCore( instance.player ) then SF.Throw( "You must be an admin to use this function!" ) end
     
     checktype( self, ply_meta )
-    local ent = getPly( self )
-    ent:SetRunSpeed( runSpeed )
+    local ply = getPly( self )
+    ply:SetRunSpeed( runSpeed )
 end
 
 
 --- Sets the gravity multiplier of a player. Passing 0 will reset it to its default value of 1.
 -- @param number Gravity
 function player_methods:setGravity( gravity )
-    if not CanUsePlyCore(instance.player) then SF.Throw("You must be an admin to use this function!") end
+    if not canUsePlyCore( instance.player ) then SF.Throw( "You must be an admin to use this function!" ) end
     
     checktype( self, ply_meta )
-    local ent = getPly( self )
-    ent:SetGravity( gravity )
+    local ply = getPly( self )
+    ply:SetGravity( gravity )
 end
 
 
 --- Sets the position of a player.
 -- @param vector Position
 function player_methods:setPos( pos )
-    if not CanUsePlyCore(instance.player) then SF.Throw("You must be an admin to use this function!") end
+    if not canUsePlyCore( instance.player ) then SF.Throw( "You must be an admin to use this function!" ) end
     
     checktype( self, ply_meta )
-    local ent = getPly( self )
+    local ply = getPly( self )
     local position = vunwrap(pos)
-    ent:SetPos( position )
+    ply:SetPos( position )
 end
 
 
 --- Sets the eye angles of a player.
 -- @param angle EyeAngles
 function player_methods:setEyeAngles( ang )
-    if not CanUsePlyCore(instance.player) then SF.Throw("You must be an admin to use this function!") end
+    if not canUsePlyCore( instance.player ) then SF.Throw( "You must be an admin to use this function!" ) end
     
     checktype( self, ply_meta )
-    local ent = getPly( self )
+    local ply = getPly( self )
     local angle = aunwrap(ang)
-    ent:SetEyeAngles( angle )
+    ply:SetEyeAngles( angle )
 end
 
 
 --- Strips all ammo from a player.
 function player_methods:stripAmmo()
-    if not CanUsePlyCore(instance.player) then SF.Throw("You must be an admin to use this function!") end
+    if not canUsePlyCore( instance.player ) then SF.Throw( "You must be an admin to use this function!" ) end
     
     checktype( self, ply_meta )
-    local ent = getPly( self )
-    ent:StripAmmo()
+    local ply = getPly( self )
+    ply:StripAmmo()
 end
 
 
@@ -163,12 +163,12 @@ end
 -- @param any AmmoID
 -- @param number Amount
 function player_methods:setAmmo( ammoId, amount )
-    if not CanUsePlyCore(instance.player) then SF.Throw("You must be an admin to use this function!") end
-    if type(ammoId) ~= "string" and type(ammoId) ~= "number" then SF.Throw("Ammo ID must either be a string or a number!") end
+    if not canUsePlyCore( instance.player ) then SF.Throw( "You must be an admin to use this function!" ) end
+    if type( ammoId ) ~= "string" and type( ammoId ) ~= "number" then SF.Throw( "Ammo ID must either be a string or a number!" ) end
     
     checktype( self, ply_meta )
-    local ent = getPly( self )
-    ent:SetAmmo( amount, ammoId )
+    local ply = getPly( self )
+    ply:SetAmmo( amount, ammoId )
 end
 
 
@@ -177,34 +177,34 @@ end
 -- @param number Amount
 -- @param bool HidePopup
 function player_methods:giveAmmo( ammoId, amount, hidePopup )
-    if not CanUsePlyCore(instance.player) then SF.Throw("You must be an admin to use this function!") end
+    if not canUsePlyCore(instance.player) then SF.Throw("You must be an admin to use this function!") end
     if type(ammoId) ~= "string" and type(ammoId) ~= "number" then SF.Throw("Ammo ID must either be a string or a number!") end
     
     checktype( self, ply_meta )
-    local ent = getPly( self )
-    ent:GiveAmmo( amount, ammoId, hidePopup )
+    local ply = getPly( self )
+    ply:GiveAmmo( amount, ammoId, hidePopup )
 end
 
 
 --- Sets how much ammo the weapon has in its primary clip. It is possible to exceed the weapon's regular max clip ammount with this function.
 -- @param number Ammo
 function weapon_methods:setClip1( amount )
-    if not CanUsePlyCore(instance.player) then SF.Throw("You must be an admin to use this function!") end
+    if not canUsePlyCore(instance.player) then SF.Throw("You must be an admin to use this function!") end
     
     checktype( self, wep_meta )
-    local ent = wunwrap(self)
-    ent:SetClip1( amount )
+    local ply = wunwrap(self)
+    ply:SetClip1( amount )
 end
 
 
 --- Sets how much ammo the weapon has in its secondary clip.
 -- @param number Ammo
 function weapon_methods:setClip2( amount )
-    if not CanUsePlyCore(instance.player) then SF.Throw("You must be an admin to use this function!") end
+    if not canUsePlyCore(instance.player) then SF.Throw("You must be an admin to use this function!") end
     
     checktype( self, wep_meta )
-    local ent = wunwrap(self)
-    ent:SetClip2( amount )
+    local ply = wunwrap(self)
+    ply:SetClip2( amount )
 end
 
 
