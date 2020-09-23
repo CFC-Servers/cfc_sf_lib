1d33491b5e056c9b15d05b6da191fa562e8e6ed9
= <ProfessorBoxtrot@gmail.com>
2020-09-23 13:44:38 -0700

    Add server/shared ply stuff

---
+++ lua/starfall/libs_sh/ply.lua
@@ -0,0 +1,95 @@
+return function( instance )
+
+
+local checktype = instance.CheckType
+local player_methods = instance.Types.Player.Methods
+local weapon_methods = instance.Types.Weapon.Methods
+local ply_meta, punwrap = instance.Types.Player, instance.Types.Player.Unwrap
+local builtins_library = instance.env
+
+
+local function getPly( this )
+    local ent = punwrap( this )
+
+    if ent:IsValid() then
+        return ent
+    else
+        SF.Throw( "Player is not valid.", 3 )
+    end
+end
+
+
+local function CanUsePlyCore( Play )
+    return Play:IsAdmin() or Play:IsSuperAdmin()
+end
+
+
+--- Gets the slow walking speed of a player. This is when you're using +WALK.
+-- @return Slow-walk speed of the player
+function player_methods:getSlowWalkSpeed()
+    if not CanUsePlyCore(instance.player) then SF.Throw("You must be an admin to use this function!") end
+    
+    checktype( self, ply_meta )
+    local ent = getPly( self )
+    
+    return ent:GetSlowWalkSpeed()
+end
+
+
+--- Gets the regular walking speed of a player. This is when you're just holding W/A/S/D and not using +WALK.
+-- @return Walk speed of the player
+function player_methods:getWalkSpeed()
+    checktype( self, ply_meta )
+    local ent = getPly( self )
+    
+    return ent:GetWalkSpeed()
+end
+
+
+--- Gets the regular crouched walking speed of a player. This is a multiplier from 0-1.
+-- @return Crouched walk speed multiplier of the player
+function player_methods:getCrouchSpeedMultiplier()
+    checktype( self, ply_meta )
+    local ent = getPly( self )
+    
+    return ent:GetCrouchedWalkSpeed()
+end
+
+
+--- Gets the gravity multiplier of a player.
+-- @return Gravity of the player
+function player_methods:getGravity()
+    checktype( self, ply_meta )
+    local ent = getPly( self )
+    
+    return ent:GetGravity()
+end
+
+
+--- Gets the ammo table of a specific player, displaying all their ammo they currently have. Similar to Player:getAmmoCount(), but returns a table of all their ammo instead of just one.
+-- @return A table containing all the player's ammo
+function player_methods:getAmmo()
+    checktype( self, ply_meta )
+    local ent = getPly( self )
+    
+    return ent:GetAmmo()
+end
+
+
+--- Gets the name (string ID) of an ammo, given an ID. Refer to the keys given from the Player:getAmmo() table for numeric IDs.
+-- @param number ID
+-- @return The name of the given ammo ID
+function builtins_library.getAmmoName( id )
+    return game.GetAmmoName(id)
+end
+
+
+--- Gets the ID (numerical ID) of an ammo, given its name. Inverse of getAmmoName(id)
+-- @param string Name
+-- @return The ID of the given ammo name
+function builtins_library.getAmmoID( name )
+    return game.GetAmmoID(name)
+end
+
+
+end
