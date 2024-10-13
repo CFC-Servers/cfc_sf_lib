4a89b2fea064cc73bdf9394ff09861ed33f10b9e
StrawWagen <64710817+StrawWagen@users.noreply.github.com>
2024-10-13 16:13:17 -0700

    color > colorV

--- lua/starfall/libs_sv/ply.lua
+++ lua/starfall/libs_sv/ply.lua
@@ -151,14 +151,14 @@ end
 
 --- Sets the player color of a player.
 -- @param vector PlayerColor
-function player_methods:setPlayerColor( color )
+function player_methods:setPlayerColor( colorV )
     checkPlyCorePerms( instance.player )
 
     checktype( self, ply_meta )
 
     local ply = getPly( self )
-    local playerColor = vunwrap( color ) -- SetPlayerColor takes a vector with x,y,z being between 0-1
-    ply:SetPlayerColor( playerColor )
+    local playerColorV = vunwrap( colorV ) -- SetPlayerColor takes a vector with x,y,z being between 0-1
+    ply:SetPlayerColor( playerColorV )
 end
 
 --- Sets the model of a player.
